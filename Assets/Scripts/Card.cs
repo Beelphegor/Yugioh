f4fdc937a08a4818c2c02ce27baadb8444783ab1
Juan Jose Guevara <juanjose@acklenavenue.com>
2014-02-25 00:27:51 -0600

    intentando instanciar cartas en el tablero

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour {
+
+	public string name;
+	public int attack;
+	public int defense;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Cards/LOB002.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LOB002 : Card {
+	public LOB002(){
+		name = "Hitotsu-Me Giant";
+		attack = 1200;
+		defense =1000;
+		//SpriteRenderer sr = new SpriteRenderer ();
+	}
+
+
+}
